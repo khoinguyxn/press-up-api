99438e10065310a3d8f6e598f8cdc218c7ed3c6b
Khoi Nguyen <khoinm2003@gmail.com>
2025-07-06 23:32:47 +1000

    Fix: Switch to Dockerfile builder

--- PressUpAspireApp.Api/Program.cs
+++ PressUpAspireApp.Api/Program.cs
@@ -27,17 +27,17 @@ string[] summaries =
 app.MapGet("/weatherforecast", () =>
                                {
                                    var forecast = Enumerable.Range(1, 5).Select(index =>
-                                                                                    new WeatherForecast
-                                                                                        (
-                                                                                         DateOnly
-                                                                                            .FromDateTime(DateTime
-                                                                                                .Now
-                                                                                                .AddDays(index)),
-                                                                                         Random.Shared
-                                                                                            .Next(-20, 55),
-                                                                                         summaries
-                                                                                             [Random.Shared.Next(summaries.Length)]
-                                                                                        ))
+                                                                                            new WeatherForecast
+                                                                                                (
+                                                                                                 DateOnly
+                                                                                                    .FromDateTime(DateTime
+                                                                                                                 .Now
+                                                                                                                 .AddDays(index)),
+                                                                                                 Random.Shared
+                                                                                                    .Next(-20, 55),
+                                                                                                 summaries
+                                                                                                     [Random.Shared.Next(summaries.Length)]
+                                                                                                ))
                                                             .ToArray();
                                    return forecast;
                                })
